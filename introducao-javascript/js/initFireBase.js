2e814114cd1fb63e52f48201d7d0c64c6f3c20cb
Jose Carlos <jose.carlos@systemsmobile.com.br>
2017-06-29 10:26:11 -0300

    criando

---
+++ introducao-javascript/js/initFireBase.js
@@ -0,0 +1,12 @@
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA7gTxONTW3X5USbc9txDYDl6yTSsBc9GM",
+    authDomain: "nutricaoteste-c2f96.firebaseapp.com",
+    databaseURL: "https://nutricaoteste-c2f96.firebaseio.com",
+    projectId: "nutricaoteste-c2f96",
+    storageBucket: "nutricaoteste-c2f96.appspot.com",
+    messagingSenderId: "110895461351"
+  };
+  firebase.initializeApp(config);
+</script>
